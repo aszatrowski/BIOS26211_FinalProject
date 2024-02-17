22e088d98634d762d72157a789893a4bc4740454
Austin Szatrowski <austinszatrowski@gmail.com>
2024-02-17 17:51:24 -0600

    finished tidying with cad_status

--- preprocessing.r
+++ preprocessing.r
@@ -9,13 +9,19 @@ gse_20680 <- read.table(paste0(path, "GSE20680_no-metadata.txt"),
     fill = TRUE,
     nrow = 45300)
 
-gse_20680_labels = read.table(paste0(path, "GSE20680_labels.txt"),
+gse_20680_labels <- read.table(paste0(path, "GSE20680_labels.txt"),
     sep = "\t",
     header = FALSE,
-    quote = "",
+    #quote = "\"",
     skip = 0,
     fill = TRUE,
     col.names = "cad_status")
+
+gse_20680_labels_vec <- gsub(
+    pattern = "disease state: ",
+    replacement = "",
+    gse_20680_labels$cad_status
+)
 
 # remove auto-added X. pattern
 colnames(gse_20680) <- gsub(pattern = "X.", replacement = "", x = colnames(gse_20680))
@@ -27,8 +33,8 @@ colnames(gse_20680) <- gsub(pattern = "\\.", replacement = "", x = colnames(gse_
 gse_20680_tidy <- as_tibble(t(gse_20680[1:45015,2:196]))
 colnames(gse_20680_tidy) <- paste0("g", colnames(gse_20680_tidy))
 gse_20680_tidy <- gse_20680_tidy %>%
-    mutate(sample_id = colnames(gse_20680)[-1]) %>%
-    select(sample_id, g1:g45015)
+    mutate(sample_id = colnames(gse_20680)[-1], cad_status = gse_20680_labels_vec) %>%
+    select(sample_id, cad_status, g1:g45015)
 
 # columns: REF_ID for numeric gene index, then 195 columns for individuals, then cad_status for CAD status
 write_csv(gse_20680_tidy, "../GEOData/GSE20680.csv")
